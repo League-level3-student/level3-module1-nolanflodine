c25e147dfd84171ce80ae04032c2b485a743ac45
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-05-14 08:26:09 -0700

    ArrayLists

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,11 +2,12 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	//1. Create a private int array. Don't initialize it.
-
+private int[] num;
 	
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
+		num = new int[0];
 	
 	}
 	
@@ -14,21 +15,26 @@ public class DynamicIntegerArray {
 	public void add(int v) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] num2 = new int[num.length+1];
 		//B. set the last element of your new array to 
 		//   the value passed into the method
-		
+		num2[num2.length-1] = v;
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
-		
+		for (int i = 0; i < num.length; i++) {
+			num2[i] = num[i];
+		}
 		//D. set the member array equal to the new array.
+		for (int n = 0; n < num2.length; n++) {
+			num[n] = num2[n];
+		}
 	}
 	
 	//4. Complete the steps in the get method
 	public int get(int location) {
 		//A. Return the value of the memeber array at the location passed in
-		return 0;
+		return num[location];
 	}
 	
 	//5. Run the DynamicArrayTest to see if you are correct so far.
--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,18 +6,35 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+ArrayList<String> word = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+word.add("Apple");
+word.add("Dog");
+word.add("the");
+word.add("cat");
+word.add("broski");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < word.size(); i++) {
+			System.out.println(word.get(i));
+		}
 		
 		//4. Print all the Strings using a for-each loop
-		
+		for(String s : word) {
+			System.out.println(s);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < word.size(); i=i+2) {
+			System.out.println(word.get(i));
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for (int i = word.size()-1; i >= 0; i--) {
+			System.out.println(word.get(i));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < word.size(); i++) {
+			if(word.get(i).contains("e")) {
+				System.out.println(word.get(i));
+			}
+		}
 	}
 }
